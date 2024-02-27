5304047d7cbb20af51f4c4161456bda4613145ce
Mollacoading <ashimmolla25@gmail.com>
2024-02-27 12:41:46 +0600

    This is my Firtst Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+const host = "127.0.0.1";
+
+
+const routeHandeler = (fileName, statusCode, req, res) =>{
+    fs.readFile(fileName, (err, data)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.writeHead(statusCode, "utf-8", {'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        }
+    });
+}
+
+
+
+const myServer = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        routeHandeler("./viwes/home.html", 200, req, res);
+    }
+    else if(req.url === '/.about'){
+        routeHandeler("./viwes/about.html", 200, req, res);
+    }
+    else{
+        routeHandeler("./viwes/contact.html", 200, req, res);
+    }
+})
+    ;
+myServer.listen(port, host, (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(`Your Account is Running at http://${host}:${port}`)
+    }
+});
